10491ded169ab0c48257577f3c591eb7424d41f7
patrifdz <fdzpatri@gmail.com>
2024-11-27 23:39:34 +0100

    feat: get data from form's fields of transfer page and aply validators

--- src/pages/transfer/transfer.js
+++ src/pages/transfer/transfer.js
@@ -11,16 +11,118 @@ getAccountList().then( account => {
 setAccountOptions (account, paramsUrl.id)
 })
 
+
 let transfer = {
     iban: '',
-    beneficiario: '',
-}
+    name: '',
+    amount: '',
+    concept: '',
+    notes: '',
+    date: {
+        day: '',
+        month: '',
+        year: '',
+    },
+    email: ''
+};
 
 onUpdateField('iban', event => {
     const value = event.target.value;
-    login = { ...transfer, iban: value };
-   
-    formValidation.validateField('name', transfer.beneficiario).then(result => {
-        onSetError('name', result);
+    transfer = { ...transfer, iban: value };
+    formValidation.validateField('iban', transfer.iban).then(result => {
+        onSetError('iban', result);
         });
 });
+
+onUpdateField('name', event => {
+    const value = event.target.value;
+    transfer = { ...transfer, name: value };
+    formValidation.validateField('name', transfer.name).then(result => {
+    onSetError('name', result);
+    });
+});
+
+onUpdateField ('amount', event =>{
+    const value = event.target.value;
+    transfer = {...transfer, amount: value}
+    formValidation.validateField('amount', transfer.amount).then(result => {
+        onSetError('amount', result);
+        });
+} )
+
+onUpdateField ('concept', event =>{
+    const value = event.target.value;
+    transfer = {...transfer, concept: value}
+    formValidation.validateField('concept', transfer.concept).then(result => {
+        onSetError('concept', result);
+        });
+} )
+
+onUpdateField ('notes', event =>{
+    const value = event.target.value;
+    transfer = {...transfer, notes: value}
+    formValidation.validateField('notes', transfer.notes).then(result => {
+        onSetError('notes', result);
+        });
+} )
+
+onUpdateField('day', event => {
+    const value = event.target.value;
+    
+    transfer = {
+        ...transfer,
+        date: {
+            ...transfer.date,
+            day: value
+        }
+    };
+    formValidation.validateField('day', transfer.date.day).then(result => {
+        onSetError('date', result);
+        });
+});
+
+
+onUpdateField ('month', event =>{
+    const value = event.target.value;
+    transfer = {
+        ...transfer,
+        date: {
+            ...transfer.date,
+            month: value
+        }
+    };
+    formValidation.validateField('month', transfer.date.month).then(result => {
+        onSetError('date', result);
+        });
+} )
+
+onUpdateField ('year', event =>{
+    const value = event.target.value;
+    transfer = {
+        ...transfer,
+        date: {
+            ...transfer.date,
+            year: value
+        }
+    };
+    formValidation.validateField('year', transfer.date.year).then(result => {
+        onSetError('date', result);
+        });
+} )
+
+
+onUpdateField ('email', event =>{
+    const value = event.target.value;
+    transfer = {...transfer, email: value}
+    formValidation.validateField('email', transfer.email).then(result => {
+        onSetError('email', result);
+        });
+} )
+
+onSubmitForm( 'transfer-button', ( ) => {
+    console.log({transfer})
+    formValidation.validateForm (transfer).then( result => {
+        console.log(result)
+        onSetFormErrors(result);
+    })
+})
--- src/pages/transfer/transfer.validations.js
+++ src/pages/transfer/transfer.validations.js
@@ -1,24 +1,45 @@
 import { Validators, createFormValidation } from '@lemoncode/fonk';
 import { iban } from '@lemoncode/fonk-iban-validator';
 
+const commonValidation = [
+    {
+        validator: Validators.required,
+        message: 'Campo requerido',
+    }
+];
 const validationSchema = {
     field: {
         iban: [
-            {
-            validator: Validators.required,
-            message: 'Campo requerido',
-            },
+            ...commonValidation,
             {
             validator: iban.validator,
-            message: 'Iban no válido',
+            message: 'IBAN no válido',
             },
         ],
-        beneficiario: [
-            {
-            validator: Validators.required,
-            message: 'Campo requerido',
-            },
-        ],
+        name  : commonValidation,
+        amount: commonValidation,
+        concept: commonValidation,
+        notes: commonValidation,
+        date : {
+            day: [
+                ...commonValidation,
+                {
+                    validator: Validators.pattern,
+                    customArgs: { pattern:  '/^(7|8|9)\d{9}$/ ' },
+                    message: 'La fecha introducida no es correcta'
+                  },
+            ],
+            month: commonValidation,
+            year: commonValidation,
+        },
+        email: [
+            ...commonValidation,
+        {
+            validator: Validators.email,
+            message: 'Email no válido',
+        }
+        ]
+
     },
 };
 
